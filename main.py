5aa403552d2a5afe84f0d79daafaa74245f82af6
NatanimA <Natanimabesha@gmail.com>
2023-05-26 03:32:00 +0300

    Chart Insertions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl as xl
+from openpyxl.chart import Reference, BarChart
 
 wb = xl.load_workbook('./transactions.xlsx')
 sheet = wb['Sheet1']
@@ -8,5 +9,18 @@ for row in range(2,sheet.max_row + 1):
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row,4)
     corrected_price_cell.value = corrected_price
+
+    values = Reference(
+        min_row=2,
+        max_row=sheet.max_row,
+        max_col=4,
+        min_col=4,
+        worksheet=sheet
+    )
+
+    chart = BarChart()
+    chart.add_data(values)
+
+    sheet.add_chart(chart,'e2')
 
 wb.save('transaction_update.xlsx')
